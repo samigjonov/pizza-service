cd63e11ed964dec45cc1285e2b5e0a573a668344
Ulugbek <samigjonov@gmail.com>
2020-08-09 12:19:09 +0500

    feat: app customized for heroku

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,18 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 // CORS is enabled
 app.use(cors);
 
+
+
 app.use('/api', pizzaRoutes);
 app.use('/api', orderRoutes);
 app.use('/api', authRoutes);
 app.use('/api', rateRoutes);
+
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+});
 
 // Handling thrown errors
 app.use(globalErrorHandler);
@@ -33,6 +41,6 @@ mongoose.connect(
     process.env.MONGO_URI,
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
-        app.listen(3500);
+        app.listen(process.env.PORT || 3500);
     })
     .catch(err => console.log(err));
